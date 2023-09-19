522fc70378d96c9bd94ea3178c6aa71ecf3dd702
Prabh <sonukaur68@gmail.com>
2023-09-18 20:13:51 -0600

    literls

--- assertEqual.js
+++ assertEqual.js
@@ -3,7 +3,7 @@ const assertEqual = function(actual, expected) {
     if ((actual  === expected)) {
         console.log('\u{1F604}'," Assertion Passed:",actual,"===",expected);
     } else {
-        console.log('\u{1F624}',"Assertion Failed:",actual,"!==",expected);
+        console.log(`'\u{1F624}',"Assertion Failed:",${actual},"!==",${expected}`);
     }
 };
 
