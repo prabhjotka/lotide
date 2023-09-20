8551ab4699809616de7ced5817705b09ae554ede
Prabh <sonukaur68@gmail.com>
2023-09-20 04:48:30 -0600

    letterposition added

---
+++ letterPositions.js
@@ -0,0 +1,36 @@
+const letterPositions = function (sentence) {
+  let results = {};
+  // logic to update results here
+
+  for (let i in sentence) {
+    const letter = sentence[i];
+
+    if (!results[letter]) {
+      results[letter] = [i];
+    } else {
+      results[letter].push(i);
+    }
+  }
+
+  return results;
+};
+
+function eqArrays(arr1, arr2) {
+   let numArr=arr1.map(Number);
+   return JSON.stringify(numArr) == JSON.stringify(arr2);
+
+}
+
+const assertArraysEqual = function (actual, expected) {
+  const result = eqArrays(actual, expected);
+
+  if (result) {
+    console.log("Assertion Matched");
+  } else {
+    console.log("Asserntion does not match");
+  }
+};
+
+//assertArraysEqual(letterPositions("hello").e, [1]);
+console.log(letterPositions("hello").l);
+assertArraysEqual(letterPositions("hello").l, [2,3]);
