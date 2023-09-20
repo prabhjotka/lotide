3e9f6cd18d3298d7d6f702f06fbfc4d9f610bbe0
Prabh <sonukaur68@gmail.com>
2023-09-20 05:17:26 -0600

    findkeyadded

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+
+const findKeyByValue=function(objects,valueofObj)
+{    
+    for(let item in objects)
+    {
+    
+        if(objects[item]===valueofObj)
+        {
+            return item;
+        }
+    }
+
+};
+const assertEqual = function(actual, expected) {
+    if ((actual  === expected)) {
+        console.log('\u{1F604}'," Assertion Passed:",actual,"===",expected);
+    } else {
+        console.log(`'\u{1F624}',"Assertion Failed:",${actual},"!==",${expected}`);
+    }
+};
+
+const bestTVShowsByGenre = { 
+    sci_fi: "The Expanse",
+    comedy: "Brooklyn Nine-Nine",
+    drama:  "The Wire"
+  };
+  
+  //console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
+  assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+  assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
