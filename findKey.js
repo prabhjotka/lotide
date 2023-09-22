cd950b3d3147837f22f1c7ec79fadb4c66afc22f
Prabh <sonukaur68@gmail.com>
2023-09-21 19:23:02 -0600

    findkey and takeuntil added

---
+++ findKey.js
@@ -0,0 +1,41 @@
+
+const findKey=(ob,callback)=>
+{
+    for(let i in ob)
+    {
+        
+       if(callback(ob[i]))
+       {
+        return i;
+       }
+
+        
+    }
+
+}
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+    if ((actual  === expected)) {
+        console.log('\u{1F604}'," Assertion Passed:",actual,"===",expected);
+    } else {
+        console.log(`'\u{1F624}',"Assertion Failed:",${actual},"!==",${expected}`);
+    }
+};
+
+assertEqual(findKey({
+    "Blue Hill": { stars: 1 },
+    "Akaleri":   { stars: 3 },
+    "noma":      { stars: 2 },
+    "elBulli":   { stars: 3 },
+    "Ora":       { stars: 2 },
+    "Akelarre":  { stars: 3 }
+  }, x => x.stars === 2),'noma'); // => "noma"
+  assertEqual(findKey({
+    "Blue Hill": { stars: 1 },
+    "Akaleri":   { stars: 3 },
+    "noma":      { stars: 2 },
+    "elBulli":   { stars: 3 },
+    "Ora":       { stars: 2 },
+    "Akelarre":  { stars: 3 }
+  }, x => x.stars === 5),'noma');
